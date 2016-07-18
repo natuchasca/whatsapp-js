2b8076394c6cdbaa44868f6aa8416a759335cb10
carol <carolina.fernandez.vela@hotmail.com>
2016-07-18 13:35:58 -0400

    primeros cambios de txt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 //NIVEL 0
 //1 Input de texto
+//variable para agregar texto en el input clase escribir-mensaje
 
-
+$('.escribir').bind('keypress', function(e) {
+	if(e.keyCode==13){
+		var txt= $(this).val();
+		$(".mensaje-respuesta1").html(txt);
+		agregar();
+	}
+});
+function agregar(){
+	var txt1= document.createElement("p");
+	$(".mensaje-respuesta1").
+}
 
 //2 Burbujas
 
