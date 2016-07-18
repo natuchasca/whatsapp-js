fbd3a0edb0a28c54c119efab7642f4a64ab7fd83
natuchasca <natuxalee@gmail.com>
2016-07-18 10:22:59 -0400

    enlazado js jquery, cdn. Sass ok

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//NIVEL 0
+//1 Input de texto
+
+
+
+//2 Burbujas
+
+
+
+
+//3Lista de Contactos
+
+
+
+
+//4Limpiar lista del chat
+
+
+
+
+
+//NIVEL 1 Memoria
+//5 cargar mensajes anteriores
+
+
+//6 Sistema de busqueda para filar Contactos
+
+
+
