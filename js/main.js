7e73c9816ec1a8e892bec8abbb3a4482624b9de4
natuchasca <natuxalee@gmail.com>
2016-07-19 13:46:19 -0400

    js alivane

--- js/main.js
+++ js/main.js
@@ -9,7 +9,48 @@
 
 
 //3Lista de Contactos
+$(document).ready(function(){
 
+	$('.cuadro-contacto').on('click',function(){
+		var thisClone = $(this).clone();
+		$('.contacto-mensaje img').add('.contacto-mensaje').add('.contacto-mensaje div').hide();
+		$('.contacto-mensaje').html(thisClone);
+		$('.contacto-mensaje').find('.estado').remove();
+		//$('.nombre-contacto').removeClass().addClass('nombre-contacto-mensaje');
+		//$('.hora-fecha').removeClass().addClass('contactos-hablando');
+		$('.contacto-mensaje .contactos-hablando').after('<div class="icons"><i class="icon-attachment icon-mensaje-up"></i><i class="icon-dots-three-vertical icon-mensaje-up"></i></div>');
+		$('.mensajes').hide();
+
+		$('.primero').click(function(){
+			$('.mensajes').show()
+		})
+		$('.segundo').click(function(){
+			console.log("soy el segundo")
+		})
+		$('.tercero').click(function(){
+			console.log("soy el tercero")
+		})
+		$('.cuarto').click(function(){
+			console.log("soy el cuarto")
+		})
+		$('.quinto').click(function(){
+			console.log("soy el quinto")
+		})
+		$('.sexto').click(function(){
+			console.log("soy el sexto")
+		})
+		$('.septimo').click(function(){
+			console.log("soy el septimo")
+		})
+		$('.octavo').click(function(){
+			console.log("soy el octavo")
+		})
+		$('.noveno').click(function(){
+			console.log("soy el noveno")
+		})
+	})
+
+})
 
 
 
@@ -21,6 +62,7 @@
 
 //NIVEL 1 Memoria
 //5 cargar mensajes anteriores
+//https://platzi.com/blog/local-storage-html5/
 
 
 
@@ -74,6 +116,7 @@ $(document).ready(function() {
 	  var val = $(this).val();
 	  $(this).siblings('span').text(val);
 	})
+
 });
 
 
