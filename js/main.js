b47037e21acc42790cb960d695951f7daecc2fc8
natuchasca <natuxalee@gmail.com>
2016-07-18 21:35:34 -0400

    js fail

--- js/main.js
+++ js/main.js
@@ -24,9 +24,65 @@
 
 
 //6 Sistema de busqueda para filar Contactos
-//Activo Categorias para seleccionar peliculas
-	$('.buscar-input').click(function() {
+//Recoger valor texto del input 
+//Comparar este valor con el nombre de contactos 
+//Mostrar los que coincidan al hacer enter
 
-	});
+ /*   var contactos = new Array();
+    contactos = $(".nombre-contacto").text();
+    var input = $(".buscar-input").val();
+
+    $(".buscar-input").on("change key paste", function(){
+        $(".cuadro-contacto").find(input);
+
+        if(input == contactos) {
+            $(".cuadro-contacto").fadeIn('slow').removeClass('hidden');
+        }
+        else {
+            $(".cuadro-contacto").fadeOut('normal').addClass('hidden');
+        }
 
 
+    });
+
+  */ 
+
+
+/*
+
+var contactos = [];
+$(".nombre-contacto").push(contactos);
+ 
+var input = $(".buscar input:text");
+    var nombre = $(".nombre-contacto").text();
+
+
+   
+    else {
+        $('.cuadro-contacto').each(function() {
+            if($(this).val == input) {
+                $(this).fadeIn('slow').removeClass('hidden');
+            }
+            else {
+                $(this).fadeOut('normal').addClass('hidden');
+            }
+        });
+    }
+*/
+
+//https://github.com/Ranks/emojify.js
+//http://hassankhan.me/emojify.js/
+/*$(".insert-emoticon").on(click, function (){
+	emojify.run()
+});
+
+emojify.run();
+// OR
+emojify.run(document.getElementById('my-element'))
+// OR
+emojify.run(null, function(emoji, emojiName){
+  var span = document.createElement('span');
+  span.className = 'emoji emoji-'  + emojiName;
+  span.innerHTML = emoji + ' replaced';
+  return span;
+});*/
